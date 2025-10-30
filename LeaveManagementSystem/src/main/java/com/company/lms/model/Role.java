791ee868147e2682627fbdbec8d37c30b02fab60
trinhthai1613-ai <trinhthai1613@gmail.com>
2025-10-30 18:01:29 +0700

    edit

--- LeaveManagementSystem/src/main/java/com/company/lms/model/Role.java
+++ LeaveManagementSystem/src/main/java/com/company/lms/model/Role.java
@@ -1,13 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.company.lms.model;
 
-/**
- *
- * @author hp
- */
 public class Role {
+    private int roleID;
+    private String roleCode;
+    private String roleName;
+    private String description;
+    private int level;
+    private boolean isActive;
     
+    public Role() {
+    }
+    
+    public int getRoleID() {
+        return roleID;
+    }
+    
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+    
+    public String getRoleCode() {
+        return roleCode;
+    }
+    
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+    
+    public String getRoleName() {
+        return roleName;
+    }
+    
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public int getLevel() {
+        return level;
+    }
+    
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    
+    public boolean isActive() {
+        return isActive;
+    }
+    
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
