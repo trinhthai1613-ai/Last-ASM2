6a9adb62751a0be16874e40081d55579ec5bfed0
trinhthai1613-ai <trinhthai1613@gmail.com>
2025-10-30 16:22:39 +0700

    Update EncodingFilter.java

--- LeaveManagementSystem/src/main/java/com/company/lms/filter/EncodingFilter.java
+++ LeaveManagementSystem/src/main/java/com/company/lms/filter/EncodingFilter.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.company.lms.filter;
 
-/**
- *
- * @author hp
- */
-public class EncodingFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
     
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        
+        chain.doFilter(request, response);
+    }
 }
