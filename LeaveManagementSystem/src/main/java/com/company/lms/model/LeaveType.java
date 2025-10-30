ff0df320a62e63fa3d5b4fb07da5f82d1dcf341c
trinhthai1613-ai <trinhthai1613@gmail.com>
2025-10-30 16:20:32 +0700

    Update LeaveType.java

--- LeaveManagementSystem/src/main/java/com/company/lms/model/LeaveType.java
+++ LeaveManagementSystem/src/main/java/com/company/lms/model/LeaveType.java
@@ -1,13 +1,88 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.company.lms.model;
 
-/**
- *
- * @author hp
- */
 public class LeaveType {
+    private int leaveTypeID;
+    private String leaveTypeCode;
+    private String leaveTypeName;
+    private String description;
+    private int defaultDaysPerYear;
+    private boolean requiresApproval;
+    private boolean isPaid;
+    private boolean allowCustomReason;
+    private boolean isActive;
     
+    public LeaveType() {
+    }
+    
+    public int getLeaveTypeID() {
+        return leaveTypeID;
+    }
+    
+    public void setLeaveTypeID(int leaveTypeID) {
+        this.leaveTypeID = leaveTypeID;
+    }
+    
+    public String getLeaveTypeCode() {
+        return leaveTypeCode;
+    }
+    
+    public void setLeaveTypeCode(String leaveTypeCode) {
+        this.leaveTypeCode = leaveTypeCode;
+    }
+    
+    public String getLeaveTypeName() {
+        return leaveTypeName;
+    }
+    
+    public void setLeaveTypeName(String leaveTypeName) {
+        this.leaveTypeName = leaveTypeName;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public int getDefaultDaysPerYear() {
+        return defaultDaysPerYear;
+    }
+    
+    public void setDefaultDaysPerYear(int defaultDaysPerYear) {
+        this.defaultDaysPerYear = defaultDaysPerYear;
+    }
+    
+    public boolean isRequiresApproval() {
+        return requiresApproval;
+    }
+    
+    public void setRequiresApproval(boolean requiresApproval) {
+        this.requiresApproval = requiresApproval;
+    }
+    
+    public boolean isPaid() {
+        return isPaid;
+    }
+    
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+    
+    public boolean isAllowCustomReason() {
+        return allowCustomReason;
+    }
+    
+    public void setAllowCustomReason(boolean allowCustomReason) {
+        this.allowCustomReason = allowCustomReason;
+    }
+    
+    public boolean isActive() {
+        return isActive;
+    }
+    
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
